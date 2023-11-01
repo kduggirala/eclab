6bd987cf532460ef81742875267d2a5e950a4b36
Kiran Duggirala <kduggirala2013@gmail.com>
2023-10-31 19:17:08 -0500

    cannapages first scrape

--- classify/eclabsF23_chicago_classify.py
+++ classify/eclabsF23_chicago_classify.py
@@ -3,11 +3,12 @@
 # IMPORTS
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 import time
-import csv
+
+
+#Reading in city data:
+cities = pd.read_csv("most_pop_cities.csv", header = None).drop_duplicates()[0].apply(lambda s : s.replace(" ", "-"))
 
 # SETUP WEBDRIVER
 c = webdriver.ChromeOptions()
@@ -17,32 +18,12 @@ driver = webdriver.Chrome(options = c)
 driver.maximize_window()
 
 """ Current strategy """
-base_url = "https://cannapages.com/listings/near/united-states/illinois/chicago/?type=listings"
-driver.get(base_url)
-soup = BeautifulSoup(driver.page_source, "lxml")
-stores = [(x.find("h3", {"slot":"title"}).find("a").text, x.find("div", {"class":"text-muted text-nowrap text-capitalize"}).text) 
-          for x in soup.find_all("div", {"data-ms-index":"listings"})]
-# stores is a list of tuples formatted as (store_name, category)
-
-
-""" Alternative strategy (work in progress)"""
-# SETUP OUTPUT DICTIONARY 
-final_dict = {"dispensaries": [], # Dispensaries
-              "doctors": [], # Doctors
-              "smoke-shops": [], # Smoke+Shops
-              "grow-stores": [], # Grow+Stores
-              "cbd-hemp-stores": [], # CBD%2FHemp+Stores
-              "kratom-herbal": [], # Kratom%2FHerbal+Shops
-              "cbd-massage": [], # CBD+Massage
-              "cannabis-education": [], # Cannabis+Education
-              "lounges": [], # Lounges
-              "vape-shops": []} # Vape+Shops
-
-base_url = "https://cannapages.com/"
-ext = "/near/united-states/illinois/chicago/?type=listings&mapView=true"
-
-for cat in final_dict.keys:
-    driver.get(base_url + cat + ext)
+base_url = "https://cannapages.com/listings/near/"
+for city in cities:
+    driver.get(base_url + city)
+    time.sleep(0.25)
     soup = BeautifulSoup(driver.page_source, "lxml")
-    name_lst = [x.find("a").text for x in soup.find_all("h3", {"slot":"title"})]
-    final_dict[cat] = name_lst
+    stores = [(x.find("h3", {"slot":"title"}).find("a").text, x.find("div", {"class":"text-muted text-nowrap text-capitalize"}).text) 
+          for x in soup.find_all("div", {"data-ms-index":"listings"})]
+
+pd.DataFrame(stores).drop_duplicates().rename({0 : "Store Name", 1 : "Type"}, axis = 1).to_csv("cannapages.csv", index = False)
